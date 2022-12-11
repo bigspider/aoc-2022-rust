cf162eee83ad668b71c7ef47866318f423d1a12c
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2022-12-11 11:17:45 +0100

    avoid warnings for variable names

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -2,13 +2,13 @@
 use std::{io::{Stdin, BufRead}};
 
 pub fn part1(stdin: Stdin) {
-    let mut X: i32 = 1;
+    let mut x: i32 = 1;
     let mut cycle: usize = 1;
     let mut result = 0;
 
-    fn tick(result: &mut i32, X: i32, cycle: &mut usize) {
+    fn tick(result: &mut i32, x: i32, cycle: &mut usize) {
         if *cycle % 40 == 20 && *cycle <= 220 {
-            *result += (*cycle as i32) * X;
+            *result += (*cycle as i32) * x;
         }
         *cycle += 1;
     }
@@ -19,14 +19,14 @@ pub fn part1(stdin: Stdin) {
 
         match parts[0] {
             "noop" => {
-                tick(&mut result, X, &mut cycle);
+                tick(&mut result, x, &mut cycle);
             }
             "addx" => {
-                let V: i32 = parts[1].parse().expect("Failed to parse operand");
+                let v: i32 = parts[1].parse().expect("Failed to parse operand");
 
-                tick(&mut result, X, &mut cycle);
-                tick(&mut result, X, &mut cycle);
-                X += V;
+                tick(&mut result, x, &mut cycle);
+                tick(&mut result, x, &mut cycle);
+                x += v;
             }
             _ => panic!("Unexpected opcode")
         }
@@ -37,11 +37,11 @@ pub fn part1(stdin: Stdin) {
 
 
 pub fn part2(stdin: Stdin) {
-    let mut X: i32 = 1;
+    let mut x: i32 = 1;
     let mut cycle: usize = 1;
 
-    fn tick(X: i32, cycle: &mut usize) {
-        if (X - ((*cycle - 1) % 40) as i32).abs() <= 1 {
+    fn tick(x: i32, cycle: &mut usize) {
+        if (x - ((*cycle - 1) % 40) as i32).abs() <= 1 {
             print!("#");
         } else {
             print!(".");
@@ -60,14 +60,14 @@ pub fn part2(stdin: Stdin) {
 
         match parts[0] {
             "noop" => {
-                tick(X, &mut cycle);
+                tick(x, &mut cycle);
             }
             "addx" => {
-                let V: i32 = parts[1].parse().expect("Failed to parse operand");
+                let v: i32 = parts[1].parse().expect("Failed to parse operand");
 
-                tick(X, &mut cycle);
-                tick(X, &mut cycle);
-                X += V;
+                tick(x, &mut cycle);
+                tick(x, &mut cycle);
+                x += v;
             }
             _ => panic!("Unexpected opcode")
         }
